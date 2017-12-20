28dd21f0a30dba67c57568ca700e043eba4d2ec8
seanOfficeUbuntu <chenxiaoyong525@hotmail.com>
2017-12-20 17:53:18 -0500

    try component

---
+++ src/components/SignUp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="signup-container">
+      <h2>SignUp Page</h2>
+      <form>
+          <input type="text" class="signup-username" placeholder="Please enter username!" />
+          <input type="password" class="signup-password" placeholder="Please enter password!" />
+          <button class="btn btn-success signup-submit-btn">Sign Up</button>
+          <router-link to="/login">Login</router-link>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp'
+}
+</script>
