4a06ad6558100d4772406e546649acad5315deb3
seanOfficeUbuntu <chenxiaoyong525@hotmail.com>
2017-12-21 17:50:26 -0500

    basic router

---
+++ src/components/partials/FooterBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="footer-bar clearfix">
+    <nav class="navbar navbar-dark navbar-fixed-bottom bg-primary">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+          Company
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+          Community
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+          Developer
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+          Resource
+        </div>
+      </div>
+      <div class="row">
+         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&copy;2017 Random Door</div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterBar'
+}
+</script>
